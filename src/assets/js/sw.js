18029d393debdbcb5618954e5ead563ad72a0803
Henry Le <35796147+HenryLeNgocThang@users.noreply.github.com>
2020-08-06 22:42:09 +0200

    rev

--- src/assets/js/sw.js
+++ src/assets/js/sw.js
@@ -9,27 +9,38 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
         });
 
         if (('Notification' in window)) {
-            if (Notification.requestPermission().then()) {
-                notification = new Notification('Simple TODO App', {
-                    body: "Missed TODOs: " + 21 + ".\nTODOs today: " + 399,
-                    icon: "/assets/img/logo.png",
-                    badge: "/assets/img/icons/logo-192x192.png",
-                });
+            Notification.requestPermission().then(permissionResult => {
+                if (permissionResult === "granted") {
+                    displayNotification();
+                } else {
+                    console.log("Notification permission status: " + permissionResult);
+                }
+            })
 
-                Notification.requestPermission()
-                    .then((permission) => {
-                        if (!('permission' in Notification)) {
-                            Notification.permission = permission;
-                            console.log("My logg here: " + permission);
-                        }
-                    })
-            } else {
-                Notification.requestPermission(function (permission) {
-                    if (!('permission' in Notification)) {
-                        Notification.permission = permission;
-                        console.log("My logg here: " + permission);
+            function displayNotification() {
+                const dateToday = new Date().toISOString().substring(0, 10);
+                let todos = localStorage.getItem('simple-todos') != null ? [...JSON.parse(localStorage.getItem(
+                    'simple-todos'))] : [];
+                let past = 0;
+                let today = 0;
+
+                for (let i = 0; i < todos.length; i++) {
+                    const todo = todos[i];
+
+                    if (todo.date < dateToday) {
+                        past++;
+                    } else if (todo.date == dateToday) {
+                        today++;
                     }
-                });
+
+                    if (i == todos.length - 1) {
+                        notification = new Notification('Simple TODO App', {
+                            body: "Missed TODOs: " + past + ".\nTODOs today: " + today,
+                            icon: "/assets/img/logo.png",
+                            badge: "/assets/img/icons/logo-192x192.png",
+                        });
+                    }
+                }
             }
         } else {
             console.log("Notifications not supported!");
@@ -41,6 +52,12 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
                 notification.close();
             }
         });
+
+        ///////////////////////////////
+
+        /*
+            Notification via service worker
+        */
 
         // navigator.serviceWorker.ready.then((serviceWorker) => {
         //     const pushManagerOptions = {
